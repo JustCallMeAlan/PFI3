41e17c49fab5a3e2b45976a48511c4cfdaf68fbe
Alan <Alan@Zero>
2011-04-05 23:04:42 +0200

    Signed-off-by: Alan <Alan@Zero>

---
+++ pfi3_assignment_2/src/se/mah/k3/cheng/alan/pfi3_assignment_2/pfi3_assignment_2.java
@@ -0,0 +1,13 @@
+package se.mah.k3.cheng.alan.pfi3_assignment_2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class pfi3_assignment_2 extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
